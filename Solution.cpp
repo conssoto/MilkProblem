9107298376d355cde9ecb9304a96be10628b9499
Constanza Soto <constanza.soto.12@sansano.usm.cl>
2018-11-09 16:45:52 -0300

    clase solution + Destructors

---
+++ Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Constanza Soto on 09-11-18.
+//
+
+#include "Solution.h"
---
+++ Solution.h
@@ -0,0 +1,14 @@
+//
+// Created by Constanza Soto on 09-11-18.
+//
+
+#ifndef NEWMCPB_SOLUTION_H
+#define NEWMCPB_SOLUTION_H
+
+
+class Solution {
+
+};
+
+
+#endif //NEWMCPB_SOLUTION_H
