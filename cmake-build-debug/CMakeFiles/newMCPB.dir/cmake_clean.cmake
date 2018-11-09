652ce89ac7bef9274755c3a4ff29e5954fbf7e12
Constanza Soto <constanza.soto.12@sansano.usm.cl>
2018-11-09 11:52:42 -0300

    commit inicial

---
+++ cmake-build-debug/CMakeFiles/newMCPB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newMCPB.dir/main.cpp.o"
+  "newMCPB.pdb"
+  "newMCPB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newMCPB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
